e9d1d0540304e1a1975e8c5a23977eddc22242c3
Sree S <sanakkayala12@gmail.com>
2022-06-22 06:47:12 +0100

    updated code from tutorial

--- server.js
+++
@@ -1,8 +0,0 @@
-const WebSocket  = require('ws')
-const server = new WebSocket().server({port:8080})
-
-server.on('connection', socket => {
-    socket.on('message',message =>{
-        socket.send(`Roger that! ${message}`)
-    });
-});
