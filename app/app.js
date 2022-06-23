2bebfaff975382c3974ab93672ff08e80ff6c83d
Sree S <sanakkayala12@gmail.com>
2022-06-23 07:44:53 +0100

    tried async

--- app/app.js
+++ app/app.js
@@ -1,21 +1,24 @@
 const { io } = require("socket.io-client");
 const socket = io('ws://localhost:8080');
-const readline = require('readline');
-
-
-const rl = readline.createInterface({
-    //prepares the input feature
-    input: process.stdin,
-    output: process.stdout
-});
+const prompt = require('prompt-sync')({sigint: true});
 
 
 socket.on('message', text => {
     console.log(text);
     //once a message is received, this code displays it and
     //awaits a response from user
-    rl.question('', message => {
-        socket.emit('message', message)
-        rl.close();
-    });
 });
+
+async function myFunction() {
+  return prompt('message: ');
+}
+
+
+async function get_input() {
+  let message = new Promise(function(resolve) {
+    resolve(prompt('message: '));
+  });
+    socket.emit('message', await message);
+
+}
+get_input();
